e9b48a2d1fb82f3698c4856cf28916cff857e26d
Leo <leomutungi19@gmail.com>
2022-09-06 01:46:40 +0300

    copleted assignemnt

--- index.js
+++ index.js
@@ -1,5 +1,69 @@
 /* Your Code Here */
 
+const createEmployeeRecord = (array) => {
+    return {
+      firstName: array[0],
+     familyName: array[1],
+     title: array[2],
+     payPerHour: array[3],
+     timeInEvents: [],
+     timeOutEvents: [],
+    }
+}
+ 
+ const createEmployeeRecords = (employee) => {
+     return employee.map(array => createEmployeeRecord(array))
+}
+ 
+    function createTimeInEvent(dateStamp){
+        const [date, hour] = dateStamp.split(' ')
+        this.timeInEvents.push({
+            type: 'TimeIn',
+            hour: parseInt(hour, 10),
+            date,
+        })
+        
+        "use strict"
+        return this;
+}
+ 
+    function createTimeOutEvent(dateStamp){
+        const [date, hour] = dateStamp.split(' ')
+    
+        this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date,
+    
+        })
+        return this
+}
+ 
+    function hoursWorkedOnDate(givenDate){
+    
+        const timeIn = this.timeInEvents.find(occurence => occurence.date === givenDate)
+        const timeOut = this.timeOutEvents.find(occurence => occurence.date === givenDate)
+        return (timeOut.hour - timeIn.hour) / 100;
+
+}
+ 
+    function wagesEarnedOnDate(givenDate){
+        const timeWorked = hoursWorkedOnDate.call (this, givenDate)
+        const wage = timeWorked * this.payPerHour
+        return parseInt(wage);
+        
+}
+        
+    function findEmployeeByFirstName (employees, firstName){
+        return employees.find(employee => employee.firstName === firstName)
+}
+ 
+ 
+    function calculatePayroll(employeeRecord){
+    return employeeRecord.reduce((total, employee) => total + allWagesFor.call(employee), 0)
+        
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
